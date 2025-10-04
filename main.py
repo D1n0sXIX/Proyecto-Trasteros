e92170fb51c59464e0f4930acaf491c2abfca41c
D1n0sXIX <alejmaman@gmail.com>
2025-10-04 14:05:07 +0200

    Re-estructuracion de la base de datos

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import Librerias.Conexion as conn
 import Librerias.ConsultasTablas as cons
 
+# Zona de pruebas
 user = input("Introduce el usuario: ")
 passw = input("Introduce contraseña: ")
+
 conexion = conn.Conectar(user, passw)
 
 cons.AddCliente(conexion)
